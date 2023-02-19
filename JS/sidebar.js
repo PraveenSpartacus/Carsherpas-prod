e936c0357691a07cc44236e97e300c021a8daa8f
PraveenSpartacus <praveenvipparthi007@gmail.com>
2023-02-19 18:31:01 +0530

    added loading animation

--- JS/sidebar.js
+++ JS/sidebar.js
@@ -10,3 +10,8 @@ sidebarBack.onclick = () => {
     document.getElementById("hero").classList.remove("sidebar-activate");
 }
 
+
+document.body.onload = (e) => {
+    console.log('loaded');
+    document.body.classList.remove('not_loaded');
+}
